0a118e4df6c095439c1bb90ec3a43cd2ce9f1b71
snvice <131165228+snvice@users.noreply.github.com>
2023-04-18 21:34:06 +0300

    Update lottie.py

--- lottie.py
+++ lottie.py
@@ -13,5 +13,11 @@ def load_lottieurl(url: str):
 lottie_url_hello = "https://assets7.lottiefiles.com/private_files/lf30_kbu3mkpv.json"
 lottie_hello = load_lottieurl(lottie_url_hello)
 
-# Display the animation with reduced dimensions
-st_lottie(lottie_hello, speed=1, width=150, height=150, key="hello")
+# Calculate the horizontal center of the page
+left_margin = (st._get_report_ctx().width - 150) / 2
+
+# Display the animation centered on the page with reduced dimensions
+st_lottie(lottie_hello, speed=1, width=150, height=150, key="hello", 
+          default_renderer="svg", # Use the SVG renderer to center the animation
+          style={"position": "relative", "left": f"{left_margin}px"})
+
