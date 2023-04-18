5407660fd85aab6f67c4e87cfb1c548c96b2ab41
snvice <131165228+snvice@users.noreply.github.com>
2023-04-18 21:49:01 +0300

    Update lottie.py

--- lottie.py
+++ lottie.py
@@ -1,4 +1,18 @@
-import streamlit as st
 
-# Set up the title with a larger font size, a custom color, and an underline
-st.markdown("<h1 style='text-align: center; color: #00FFFF; font-size: 48px; text-decoration: underline dotted;'>Scan Squad</h1>", unsafe_allow_html=True)
+import time
+import requests
+import streamlit as st
+from streamlit_lottie import st_lottie
+
+def load_lottieurl(url: str):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
+
+# Load the Lottie animation from a URL
+lottie_url_hello = "https://assets2.lottiefiles.com/packages/lf20_hdubmnhz.json"
+lottie_hello = load_lottieurl(lottie_url_hello)
+
+# Display the animation with reduced dimensions
+st_lottie(lottie_hello, speed=1, width=250, height=250, key="hello")
