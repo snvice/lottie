a6449b8ee59a47d108f6e60e5343ed520ffefb90
snvice <131165228+snvice@users.noreply.github.com>
2023-04-18 21:30:27 +0300

    Update lottie.py

--- lottie.py
+++ lottie.py
@@ -13,7 +13,11 @@ lottie_url_hello = "https://assets7.lottiefiles.com/private_files/lf30_kbu3mkpv.
 lottie_hello = load_lottieurl(lottie_url_hello)
 
 # Display the animation centered on the page
-st.markdown(
-    f"<div style=align-items: center;'>"
-    f"{st_lottie(lottie_hello, speed=1, width=300, height=300)}"
-    + "</div>",unsafe_allow_html=True,)
+st_lottie(
+    lottie_hello,
+    speed=1,
+    width=300,
+    height=300,
+    key='hello',
+    style={'display': 'flex', 'justify-content': 'center', 'align-items': 'center'}
+)
