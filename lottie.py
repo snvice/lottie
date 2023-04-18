cc2d80982130433f215ce6824282f7968bbdbe08
snvice <131165228+snvice@users.noreply.github.com>
2023-04-18 21:06:05 +0300

    Update lottie.py

--- lottie.py
+++ lottie.py
@@ -10,10 +10,15 @@ def load_lottieurl(url: str):
     return r.json()
 
 # Load the Lottie animation from a URL
-lottie_url_hello = "https://assets3.lottiefiles.com/packages/lf20_C67qsN3hAk.json"
+lottie_url_hello = "https://assets7.lottiefiles.com/private_files/lf30_kbu3mkpv.json"
 lottie_hello = load_lottieurl(lottie_url_hello)
 
-# Create a centered container and place the animation inside it
-container = st.beta_container()
-with container:
-    st_lottie(lottie_hello, speed=1, width=150, height=150, key="hello")
+# Display the animation with reduced dimensions and centered
+st.markdown("<div style='text-align: center;'>"
+            "<h1>Hello, Streamlit!</h1>"
+            "</div>", unsafe_allow_html=True)
+st.markdown("<div style='text-align: center;'>"
+            f"<div style='display: inline-block;'>"
+            f"{st_lottie(lottie_hello, speed=1, width=300, height=300, key='hello')}"
+            f"</div>"
+            "</div>", unsafe_allow_html=True)
