eede7baa9c2bae638f2dced7a0758a0b06936b59
snvice <131165228+snvice@users.noreply.github.com>
2023-04-18 21:13:21 +0300

    Update lottie.py

--- lottie.py
+++ lottie.py
@@ -13,8 +13,11 @@ lottie_url = "https://assets10.lottiefiles.com/packages/lf20_OQ3q3qHR.json"
 lottie_json = load_lottieurl(lottie_url)
 
 # Display the animation with reduced dimensions and centered
-st.markdown("<div style='text-align: center;'>"
-            f"<div style='display: inline-block;'>"
-            f"{st_lottie(lottie_json, speed=1, width=400, height=400, key='my_lottie')}"
-            f"</div>"
-            "</div>", unsafe_allow_html=True)
+st.markdown(
+    f"<div style='display: flex; justify-content: center; align-items: center; width: 100%; height: 100vh;'>"
+    f"<div style='width: 400px; height: 400px;'>"
+    f"{st_lottie(lottie_json, speed=1, width=400, height=400, key='my_lottie')}"
+    f"</div>"
+    "</div>",
+    unsafe_allow_html=True,
+)
