151f124fbb707bad03b0eb974d03b19ed0a90170
snvice <131165228+snvice@users.noreply.github.com>
2023-04-18 21:27:13 +0300

    Update lottie.py

--- lottie.py
+++ lottie.py
@@ -16,5 +16,4 @@ lottie_hello = load_lottieurl(lottie_url_hello)
 st.markdown(
     f"<div style='display: flex; justify-content: center; align-items: center;'>"
     f"{st_lottie(lottie_hello, speed=1, width=300, height=300)}"
-    + "</div>",
-    unsafe_allow_html=True,)
+    + "</div>",unsafe_allow_html=True,)
