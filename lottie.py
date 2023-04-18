2a2de68770a40787c4e054029f2d49216dd7866e
snvice <131165228+snvice@users.noreply.github.com>
2023-04-18 21:40:00 +0300

    Update lottie.py

--- lottie.py
+++ lottie.py
@@ -1,25 +1,4 @@
-import time
-import requests
-import streamlit as st
-from streamlit_lottie import st_lottie
 
-def load_lottieurl(url: str):
-    r = requests.get(url)
-    if r.status_code != 200:
-        return None
-    return r.json()
-
-# Load the Lottie animation from a URL
-lottie_url_hello = "https://assets7.lottiefiles.com/private_files/lf30_kbu3mkpv.json"
-lottie_hello = load_lottieurl(lottie_url_hello)
-
-# Calculate the horizontal center of the page
-left_margin = (st._get_report_ctx().width - 150) / 2
-
-# Display the animation centered on the page with reduced dimensions
-st_lottie(lottie_hello, speed=1, width=150, height=150, key="hello", 
-          default_renderer="svg", # Use the SVG renderer to center the animation
-          style={"position": "relative", "left": f"{left_margin}px"})
 
 # Set up the title with a larger font size, a custom color, and an underline
 st.markdown("<h1 style='text-align: center; color: #00FFFF; font-size: 48px; text-decoration: underline dotted;'>Scan Squad</h1>", unsafe_allow_html=True)
