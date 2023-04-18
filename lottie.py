5d37defb729b3df6503393bf70f66b57c9a865f2
snvice <131165228+snvice@users.noreply.github.com>
2023-04-18 21:40:27 +0300

    Update lottie.py

--- lottie.py
+++ lottie.py
@@ -1,4 +1,4 @@
-
+import streamlit as st
 
 # Set up the title with a larger font size, a custom color, and an underline
 st.markdown("<h1 style='text-align: center; color: #00FFFF; font-size: 48px; text-decoration: underline dotted;'>Scan Squad</h1>", unsafe_allow_html=True)
