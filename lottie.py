af264ea281a9bd92d1d576135a691446826dd74f
snvice <131165228+snvice@users.noreply.github.com>
2023-04-18 20:19:59 +0300

    Update lottie.py

--- lottie.py
+++ lottie.py
@@ -1,9 +1,11 @@
 import streamlit as st
+import requests
 from streamlit_lottie import st_lottie
 
-# Load the Lottie animation from a file
-with open('https://raw.githubusercontent.com/snvice/lottie/main/giraffe.json', 'r') as f:
-    animation_data = f.read()
+# Download the Lottie animation from a URL
+url = 'https://raw.githubusercontent.com/snvice/lottie/main/giraffe.json'
+response = requests.get(url)
+animation_data = response.text
 
 # Display the animation
 st_lottie(animation_data, speed=1, width=300, height=300, key="lottie")
