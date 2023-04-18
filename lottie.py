d54b242f73f2366a33ca135ca7195562907c8986
snvice <131165228+snvice@users.noreply.github.com>
2023-04-18 21:10:51 +0300

    Update lottie.py

--- lottie.py
+++ lottie.py
@@ -10,12 +10,10 @@ def load_lottieurl(url: str):
     return r.json()
 
 # Load the Lottie animation from a URL
-lottie_url = "https://assets7.lottiefiles.com/private_files/lf30_kbu3mkpv.json"
+lottie_url = "https://assets3.lottiefiles.com/packages/lf20_W5U2ua.json"
 lottie_json = load_lottieurl(lottie_url)
 
 # Display the animation with reduced dimensions and centered
 st.markdown("<div style='text-align: center;'>"
-            f"<div style='display: inline-block;'>"
-            f"{st_lottie(lottie_json, speed=1, width=150, height=150, key='my_lottie')}"
-            f"</div>"
+            f"{st_lottie(lottie_json, speed=1, width=300, height=300, key='my_lottie')}"
             "</div>", unsafe_allow_html=True)
