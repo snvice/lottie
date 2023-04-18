94f13405b87ca39f8253aee524a77aa7b075a4ba
snvice <131165228+snvice@users.noreply.github.com>
2023-04-18 20:40:04 +0300

    Update lottie.py

--- lottie.py
+++ lottie.py
@@ -10,7 +10,7 @@ def load_lottieurl(url: str):
     return r.json()
 
 # Load the Lottie animation from a URL
-lottie_url_hello = "https://assets9.lottiefiles.com/packages/lf20_5lTxAupekw.json"
+lottie_url_hello = "https://assets7.lottiefiles.com/private_files/lf30_kbu3mkpv.json"
 lottie_hello = load_lottieurl(lottie_url_hello)
 
 # Display the animation with reduced dimensions
