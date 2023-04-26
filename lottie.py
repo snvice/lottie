d6a3fa12f3869ec962188c6e9ddcbcecf1995a24
snvice <131165228+snvice@users.noreply.github.com>
2023-04-26 18:13:11 +0300

    Update lottie.py

--- lottie.py
+++ lottie.py
@@ -5,19 +5,9 @@ import styled_components as styled
 Title = styled.h1("""
   font-size: 3rem;
   font-weight: bold;
-  color: #333;
+  color: orange;
   text-align: center;
-  animation: fade-in 1s;
-
-  @keyframes fade-in {
-    from {
-      opacity: 0;
-    }
-    to {
-      opacity: 1;
-    }
-  }
 """)
 
 # Render the styled title component in the Streamlit app
-st.write(Title("My Animated Title"))
+st.write(Title("My Orange Title"))
