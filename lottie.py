2f1cc9488ca473e2988bc9c55c9054c51eb0a41d
snvice <131165228+snvice@users.noreply.github.com>
2023-05-01 09:34:10 +0300

    Update lottie.py

--- lottie.py
+++ lottie.py
@@ -1,13 +1,42 @@
 import streamlit as st
-import styled_components as styled
+import re
+from github import Github
+from github import InputFileContent
 
-# Define a styled title component using styled-components
-Title = styled.h1("""
-  font-size: 3rem;
-  font-weight: bold;
-  color: orange;
-  text-align: center;
-""")
+# Define GitHub credentials
+github_username = "snvice"
+github_password = "samngere123!!"
+repository_name = "lottie"
 
-# Render the styled title component in the Streamlit app
-st.write(Title("My Orange Title"))
+# Authenticate to GitHub
+g = Github(github_username, github_password)
+repo = g.get_user().get_repo(repository_name)
+
+# Define a function to remove date and sender names from text using regex
+def clean_text(text):
+    pattern = r'^\d{2}/\d{2}/\d{4}, \d{2}:\d{2} - .*?:\s'
+    return re.sub(pattern, '', text, flags=re.MULTILINE)
+
+# Define the Streamlit app
+def app():
+    st.title("Text File Uploader and GitHub Saver")
+    st.write("Upload a text file to clean its contents and save it to GitHub.")
+
+    # Create a file uploader component
+    file = st.file_uploader("Choose a text file", type=["txt"])
+
+    # If a file is uploaded, clean its contents, save it to GitHub, and display success message
+    if file is not None:
+        text = file.read().decode('utf-8')
+        cleaned_text = clean_text(text)
+
+        # Upload file to GitHub
+        file_name = file.name
+        file_content = InputFileContent(cleaned_text)
+        repo.create_file(file_name, "commit message", file_content)
+
+        st.write("File uploaded to GitHub!")
+    
+# Run the Streamlit app
+if __name__ == "__main__":
+    app()
