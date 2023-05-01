db680f209395e202b487ab8079bd2a0bb24a5084
snvice <131165228+snvice@users.noreply.github.com>
2023-05-01 10:25:11 +0300

    Update lottie.py

--- lottie.py
+++ lottie.py
@@ -44,7 +44,7 @@ def clean_text(text):
     table_data = [["Word", "Frequency"]]
     for word, count in word_counts.most_common(5):
         table_data.append([word, count])
-    st.table(table_data)
+    st.write(table_data)
 
     # Generate the word cloud
     wordcloud = WordCloud(background_color="black", 
